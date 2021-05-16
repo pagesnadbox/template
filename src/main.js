1b9a977b2ee85ddaf4c4c6155ef93d9be2e40dc0
Dobrin Dimchev <dobrin.yonkov1@gmail.com>
2021-05-16 16:51:35 +0300

    add performance config

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import "./publicPath";
 import Engine from "./Engine";
+import Vue from "vue";
 
 // new Engine();
+Vue.config.performance = true
 
 window.API = window.API || {};
 window.API.Engine = Engine;
